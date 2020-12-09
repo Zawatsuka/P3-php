765ca0d462b2b85cc4dd0ee418c9a77282e5e321
Zawatsuka <57181738+Zawatsuka@users.noreply.github.com>
2020-12-09 14:09:30 +0100

    first commit

---
+++ EXO1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXO1</title>
+</head>
+<body>
+    <?php $number = 0;
+        while($number <= 100){    
+            echo '<p>'. $number .'</p>' ;
+           $number++;
+        }
+            ?></p>
+</body>
+</html>
